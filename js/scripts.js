6466ddc9d70c40e59cc2b4cb76762322b6a609fc
StefanWellhoner <stefanwellhoner@ymail.com>
2020-06-26 11:54:28 +0200

    updated shop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
+function collapse() {
+    var x = document.getElementById("topnav");
     if (x.className === "navbar") {
         x.className += " responsive";
     } else {
         x.className = "navbar";
+    }
+}
+
+function cartDropdown() {
+    var x = document.getElementById("modal-cart");
+    var y = document.getElementById("topnav");
+    if (y.className === "navbar responsive") {
+        window.location.replace("viewcart.html");
+    } else {
+        if (x.className === "shopping-cart hide") {
+            x.className = "shopping-cart";
+        } else {
+            x.className = "shopping-cart hide";
+        }
     }
 }
